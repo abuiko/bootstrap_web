e358faf7e4807ac72cff4c9bf30050c1934ea6c1
Alena Buiko <abuiko.ma@gmail.com>
2021-07-18 14:48:17 -0400

    generate 6 recommended objects / add to cart recommended section

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -2,179 +2,182 @@ window.addEventListener("DOMContentLoaded", () => {
     displayClothes(clothes);
     filterClothes(clothes);
     sortClothes(clothes);
+
+
 })
 
 // Clothes Array
 
-const clothes = [
-    {
-        id: 1,
-        name: "Cotton Dress",
-        price: 17.99,
-        img: "/dist/img/clothes/dress.jpeg",
-        type: 'dress',
-        stars: 5,
+const clothes = [{
+    id: 1,
+    name: "Cotton Dress",
+    price: 17.99,
+    img: "/dist/img/clothes/dress.jpeg",
+    type: 'dress',
+    stars: 5,
 
-    },
-    {
-        id: 2,
-        name: "Cotton Dress",
-        price: 17.99,
-        img: "/dist/img/clothes/dress2.jpeg",
-        type: 'dress',
-        stars: 3.9,
-    },
-    {
-        id: 3,
-        name: "Cotton Dress",
-        price: 17.99,
-        img: "/dist/img/clothes/dress3.jpeg",
-        type: 'dress',
-        stars: 4.4,
-    },
-    {
-        id: 4,
-        name: "Bikini Top",
-        price: 7.99,
-        img: "/dist/img/clothes/bikini.jpeg",
-        type: 'top',
-        stars: 4.2,
-    },
-    {
-        id: 5,
-        name: "Cotton Blouse",
-        price: 12.99,
-        img: "/dist/img/clothes/blouse.jpeg",
-        type: 'top',
-        stars: 3.2,
-    },
-    {
-        id: 6,
-        name: "Hoodie",
-        price: 27.99,
-        img: "/dist/img/clothes/hoodie.jpeg",
-        type: 'top',
-        stars: 4.9,
-    },
-    {
-        id: 7,
-        name: "Jacket",
-        price: 27.99,
-        img: "/dist/img/clothes/jacket.jpeg",
-        type: 'top',
-        stars: 4,
-    },
-    {
-        id: 8,
-        name: "Pants",
-        price: 15.99,
-        img: "/dist/img/clothes/pants.jpeg",
-        type: 'bottom',
-        stars: 4.1,
-    },
-    {
-        id: 9,
-        name: "Pants",
-        price: 15.99,
-        img: "/dist/img/clothes/pants2.jpeg",
-        type: 'bottom',
-        stars: 3.8,
-    },
-    {
-        id: 10,
-        name: "Shorts",
-        price: 15.99,
-        img: "/dist/img/clothes/shorts.jpeg",
-        type: 'bottom',
-        stars: 4.6,
-    },
-    {
-        id: 11,
-        name: "Sweatshirt",
-        price: 15.99,
-        img: "/dist/img/clothes/sweatshirt.jpeg",
-        type: 'top',
-        stars: 3.1,
-    },
-    {
-        id: 12,
-        name: "Sweatshirt",
-        price: 15.99,
-        img: "/dist/img/clothes/sweatshirt2.jpeg",
-        type: 'top',
-        stars: 5,
-    },
-    {
-        id: 13,
-        name: "Top",
-        price: 15.99,
-        img: "/dist/img/clothes/top.jpeg",
-        type: 'top',
-        stars: 2.7,
-    },
-    {
-        id: 14,
-        name: "Top",
-        price: 15.99,
-        img: "/dist/img/clothes/top2.jpeg",
-        type: 'top',
-        stars: 4.2,
-    },
-    {
-        id: 14,
-        name: "Top",
-        price: 15.99,
-        img: "/dist/img/clothes/top3.jpeg",
-        type: 'top',
-        stars: 3.5,
-    },
-    {
-        id: 15,
-        name: "Top",
-        price: 15.99,
-        img: "/dist/img/clothes/top5.jpeg",
-        type: 'top',
-        stars: 4,
-    },
-    {
-        id: 16,
-        name: "Top",
-        price: 15.99,
-        img: "/dist/img/clothes/vest.jpeg",
-        type: 'top',
-        stars: 5,
-    },
-    {
-        id: 18,
-        name: "Pleated Skirt",
-        price: 13.99,
-        img: "/dist/img/clothes/skirt.jpeg",
-        type: 'bottom',
-        stars: 4.6,
-    },
-    {
-        id: 19,
-        name: "Cropped Sweatshirt",
-        price: 23.99,
-        img: "/dist/img/clothes/sweatshirt3.jpeg",
-        type: 'top',
-        stars: 4.9,
-    },
-    {
-        id: 20,
-        name: "Bodysuit",
-        price: 23.99,
-        img: "/dist/img/clothes/bodysuit.jpeg",
-        type: 'top',
-        stars: 4.1,
-    },
+},
+{
+    id: 2,
+    name: "Cotton Dress",
+    price: 17.99,
+    img: "/dist/img/clothes/dress2.jpeg",
+    type: 'dress',
+    stars: 3.9,
+},
+{
+    id: 3,
+    name: "Cotton Dress",
+    price: 17.99,
+    img: "/dist/img/clothes/dress3.jpeg",
+    type: 'dress',
+    stars: 4.4,
+},
+{
+    id: 4,
+    name: "Bikini Top",
+    price: 7.99,
+    img: "/dist/img/clothes/bikini.jpeg",
+    type: 'top',
+    stars: 4.2,
+},
+{
+    id: 5,
+    name: "Cotton Blouse",
+    price: 12.99,
+    img: "/dist/img/clothes/blouse.jpeg",
+    type: 'top',
+    stars: 3.2,
+},
+{
+    id: 6,
+    name: "Hoodie",
+    price: 27.99,
+    img: "/dist/img/clothes/hoodie.jpeg",
+    type: 'top',
+    stars: 4.9,
+},
+{
+    id: 7,
+    name: "Jacket",
+    price: 27.99,
+    img: "/dist/img/clothes/jacket.jpeg",
+    type: 'top',
+    stars: 4,
+},
+{
+    id: 8,
+    name: "Pants",
+    price: 15.99,
+    img: "/dist/img/clothes/pants.jpeg",
+    type: 'bottom',
+    stars: 4.1,
+},
+{
+    id: 9,
+    name: "Pants",
+    price: 15.99,
+    img: "/dist/img/clothes/pants2.jpeg",
+    type: 'bottom',
+    stars: 3.8,
+},
+{
+    id: 10,
+    name: "Shorts",
+    price: 15.99,
+    img: "/dist/img/clothes/shorts.jpeg",
+    type: 'bottom',
+    stars: 4.6,
+},
+{
+    id: 11,
+    name: "Sweatshirt",
+    price: 15.99,
+    img: "/dist/img/clothes/sweatshirt.jpeg",
+    type: 'top',
+    stars: 3.1,
+},
+{
+    id: 12,
+    name: "Sweatshirt",
+    price: 15.99,
+    img: "/dist/img/clothes/sweatshirt2.jpeg",
+    type: 'top',
+    stars: 5,
+},
+{
+    id: 13,
+    name: "Top",
+    price: 15.99,
+    img: "/dist/img/clothes/top.jpeg",
+    type: 'top',
+    stars: 2.7,
+},
+{
+    id: 14,
+    name: "Top",
+    price: 15.99,
+    img: "/dist/img/clothes/top2.jpeg",
+    type: 'top',
+    stars: 4.2,
+},
+{
+    id: 14,
+    name: "Top",
+    price: 15.99,
+    img: "/dist/img/clothes/top3.jpeg",
+    type: 'top',
+    stars: 3.5,
+},
+{
+    id: 15,
+    name: "Top",
+    price: 15.99,
+    img: "/dist/img/clothes/top5.jpeg",
+    type: 'top',
+    stars: 4,
+},
+{
+    id: 16,
+    name: "Top",
+    price: 15.99,
+    img: "/dist/img/clothes/vest.jpeg",
+    type: 'top',
+    stars: 5,
+},
+{
+    id: 18,
+    name: "Pleated Skirt",
+    price: 13.99,
+    img: "/dist/img/clothes/skirt.jpeg",
+    type: 'bottom',
+    stars: 4.6,
+},
+{
+    id: 19,
+    name: "Cropped Sweatshirt",
+    price: 23.99,
+    img: "/dist/img/clothes/sweatshirt3.jpeg",
+    type: 'top',
+    stars: 4.9,
+},
+{
+    id: 20,
+    name: "Bodysuit",
+    price: 23.99,
+    img: "/dist/img/clothes/bodysuit.jpeg",
+    type: 'top',
+    stars: 4.1,
+},
 
 ]
+
 
 // containers
 
 const clothesContainer = document.querySelector(".clothes");
 const signInContainer = document.querySelector(".signin-wrapper");
+const recommendedContainer = document.querySelector(".recommended");
 
 // event listeners
 const signInBtns = document.querySelectorAll(".signin-btn");
@@ -267,8 +270,8 @@ function addToFavorites(items) {
             let dataId = e.target.parentNode.parentNode.parentNode.getAttribute("data-id");
 
             // let favoriteClothes = clothes[dataId - 1];
-            // let myObj_serialized = JSON.stringify(favoriteClothes);
-            // localStorage.setItem("favorites", myObj_serialized);
+            // let myObj_serialized = ;
+            // localStorage.setItem("favorites", JSON.stringify(favoriteClothes));
 
             // let myObj_deserialized = JSON.parse(localStorage.getItem("favorites"));
             // console.log(myObj_deserialized);
@@ -277,6 +280,48 @@ function addToFavorites(items) {
 }
 
 
+// generate 6 random elements from array
+
+function generateSixRandom() {
+    let newArr = [];
+
+    for (let i = 0; i < 6; i++) {
+        let arr = clothes[Math.floor(Math.random() * 20)];
+        newArr.push(arr);
+    }
+
+
+    displayRecommended(newArr);
+}
+
+
+// display recommended clothes on cart page
+function displayRecommended(items) {
+
+    let recommended = items.map(item => {
+        return `<div class="col-2">
+        <div class="card clothes-card">
+            <div class="card-img-top">
+                <img src=${item.img} class="img-fluid" alt="${item.name} ">
+                <div class="card-icon"><i class="far fa-heart"></i></div>
+            </div>
+
+            <div class="card-body d-flex align-items-center justify-content-between">
+
+                <h5 class="card-title">${item.name}</h5>
+                <p class="card-text">${item.price}</p>
+
+            </div>
+        </div>
+    </div>`
+
+
+    })
+    let result = recommended.join("");
+    recommendedContainer.innerHTML = result;
+}
+
+generateSixRandom();
 
 // let myObj = {
 //     name: "Alena",
@@ -288,9 +333,3 @@ function addToFavorites(items) {
 
 // let myobj_deserialized = JSON.parse(localStorage.getItem("myObj"));
 // console.log(myobj_deserialized);
-
-
-
-
-
-
